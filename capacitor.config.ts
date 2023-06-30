c7623d98519981d43a334e502d53532c42bc048e
wolf-0-0 <sousa.cedric91@gmail.com>
2023-06-30 07:04:25 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Rest-O-Ranch',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
